22cd32f1c2f2f7f9e443f0b5d13230f4a82cf98f
Daniel Van Noord <sg1mash98@gmail.com>
2022-11-18 20:49:36 -0500

    Began adding comments

--- LetsMake.GithubUpdateChecker/GithubUpdateChecker.cs
+++ LetsMake.GithubUpdateChecker/GithubUpdateChecker.cs
@@ -4,30 +4,49 @@ namespace LetsMake
 {
     public class GithubUpdateChecker
     {
+        /// <summary>
+        /// The local projects or programs current version
+        /// </summary>
         public SemanticVersion CurrentVersion
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// The name of the remote repository
+        /// </summary>
         public string RemoteRepositoryName
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// The remove repository owner, often this is the user or group name
+        /// </summary>
         public string RemoteRepositoryOwner
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// All releases from the repository, will be null until the CheckForUpdates has succeeded 
+        /// </summary>
         public IReadOnlyList<Octokit.Release> Releases
         {
             get;
             private set;
         } = null;
 
+
+        /// <summary>
+        /// Creates a GithubUpdateChecker
+        /// </summary>
+        /// <param name="currentRunningVersion">The local projects or programs current version</param>
+        /// <param name="remoteRepositoryOwner">The remove repository owner, often this is the user or group name</param>
+        /// <param name="remoteRepositoryName">The name of the remote repository</param>
         public GithubUpdateChecker(SemanticVersion currentRunningVersion, string remoteRepositoryOwner, string remoteRepositoryName)
         {
             CurrentVersion = currentRunningVersion;
@@ -35,11 +54,17 @@ namespace LetsMake
             RemoteRepositoryOwner = remoteRepositoryOwner;
         }
 
+        /// <summary>
+        /// Checks the specified repository for releases. May throw based on failures to get data.
+        /// </summary>
         public async void CheckForUpdates()
         {
             Releases = await GitHubReleases.GetReleases(RemoteRepositoryOwner, RemoteRepositoryName);
         }
 
+        /// <summary>
+        /// True if any update is available (must be higher then the CurrentVersion)
+        /// </summary>
         public bool UpdateAvailable
         {
             get
@@ -48,6 +73,9 @@ namespace LetsMake
             }
         }
 
+        /// <summary>
+        /// Get the latest release from the repository
+        /// </summary>
         public Octokit.Release LatestRelease
         {
             get
@@ -56,6 +84,9 @@ namespace LetsMake
             }
         }
 
+        /// <summary>
+        /// Gets the latest pre-release from the repository
+        /// </summary>
         public Octokit.Release LatestPrerelease
         {
             get
@@ -64,6 +95,9 @@ namespace LetsMake
             }
         }
 
+        /// <summary>
+        /// True if there is a release higher then the CurrentVersion
+        /// </summary>
         public bool HasNewerRelease
         {
             get
@@ -72,6 +106,9 @@ namespace LetsMake
             }
         }
 
+        /// <summary>
+        /// True if there is a pre-release that is higher then the CurrentVersion and the LatestRelease
+        /// </summary>
         public bool HasNewerPrerelease
         {
             get
